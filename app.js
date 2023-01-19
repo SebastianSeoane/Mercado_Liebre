23dcbafd203303263446e2b73afe5f072ecdb58f
SebastianSeoane <seoane.sebastian@gmail.com>
2023-01-18 23:07:22 -0300

    mercado liebre

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
+const express = require("express");
+const app = express ();
 
-const express = require('express');
-const app = express();
+const path = require("path");
 
-const path = require('path');
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath)) 
+const publicPath = path.resolve(__dirname, "./public");
+app.use( express.static(publicPath) );
 
-
-app.get('/',(req,res) =>{
-    res.sendFile(path.join(__dirname,'./views/home.html'));
-});
-
-app.get('/register',(req,res) =>{
-    res.sendFile(path.join(__dirname,'./views/register.html'));
-});
-
-app.get('/login',(req,res) =>{
-    res.sendFile(path.join(__dirname,'./views/login.html'));
-});
-
-
-//app.listen(8000, () => console.log("Servidor funcionando en puerto 3001"));
 const port = process.env.PORT || 3001;
-app.listen(port,()=>console.log("Servidor corriendo en el puerto ${port}"))
+app.listen(port, () =>{ console.log(`Servidor corriendo exitosamente en puerto ${port}`)
+});
 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
 
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
 
-
-
-
-
-
-
-
-
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
